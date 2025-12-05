eed1e97bd57fb943874d75c425b8a36679def5db
MashrurBinMorshed <mashrurbinmorshed2000@gmail.com>
2025-12-05 22:34:59 +0600

    Minor Fix

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -56,15 +56,6 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
-// Validate ../../../app/terms/page.tsx
-{
-  type __IsExpected<Specific extends AppPageConfig<"/terms">> = Specific
-  const handler = {} as typeof import("../../../app/terms/page.js")
-  type __Check = __IsExpected<typeof handler>
-  // @ts-ignore
-  type __Unused = __Check
-}
-
 // Validate ../../../app/api/submit-candidate/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/submit-candidate">> = Specific
